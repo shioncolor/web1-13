9ca6286f1e535a03e774d7904fbd4132561626e9
shioncolor <czamy00@gmail.com>
2017-07-03 19:06:35 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -81,23 +81,6 @@ for(idx in thmubs){
   }
 }
 
-function getFileName()
-{
-return window.location.href.split('/').pop();
-}
-
-var filename=getFileName();
-var opt;
-if(filename==='other.html'){
-opt=document.querySelector('option[value="other.html"]');
-}
-else if(filename==='other2.html'){
-opt=document.querySelector('option[value="other2.html"]');
-}else{
-  opt=document.querySelector('option[value="index.html"]');
-}
-opt.selected=true;
-
 document.getElementById('form').select.onchange=function(){
 location.href=document.getElementById('form').select.value;
 }
