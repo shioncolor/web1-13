4f5aea8db3678e97ff204652c775250ef3ffbfb7
shioncolor <czamy00@gmail.com>
2017-07-03 18:57:06 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -62,10 +62,6 @@ function getCookie(c_name){
   return "";
 }
 
-document.getElementById('form').select.onchange=function(){
-location.href=document.getElementById('form').select.value;
-}
-
 var last_date=getCookie('lastDate');
 if(last_date){
 document.getElementById('cookie').textContent='前回訪れた時間:'+last_date;
@@ -86,3 +82,26 @@ for(idx in thmubs){
   document.getElementById("bigimg").src='img/'+this.dataset.image+'.jpg';
   }
 }
+
+function getFileName()
+{
+return window.location.href.split('/').pop();
+}
+
+var filename=getFileName();
+var opt;
+if(filename==='other.html'){
+opt=document.querySelector('option[value="other.html"]');
+}
+else if(filename==='other2.html'){
+opt=document.querySelector('option[value="other2.html"]');
+}else{
+  opt=document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
+
+document.getElementById('form').select.onchange=function(){
+location.href=document.getElementById('form').select.value;
+}
+
+
