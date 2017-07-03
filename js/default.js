654224c0986d0e9e5589bb798e95164a92cac4a6
shioncolor <czamy00@gmail.com>
2017-07-03 18:48:53 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,8 +8,11 @@ var year=time.getFullYear();
 return[sec,min,hours,days,month,year];
 }
 
+var update=function(){
 var now = new Date();
-var counter=separate_time(now);
+  var target=new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime()-now.getTime();
+var counter=separate_time(diff);
 document.getElementById('countdown').textContent =
   counter[5]+'年'+
   counter[4]+'月'+
@@ -17,6 +20,14 @@ document.getElementById('countdown').textContent =
   counter[2]+'時'+
   counter[1]+'分'+
   counter[0]+'秒';
+refresh();
+}
+
+var refresh=function()
+{
+setTimeout(update,1000);
+}
+update();
 
 function setCookie(c_mane,value,expiredays){
   var extime=new Date().getTime();
