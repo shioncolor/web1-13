835756fe48d1cdcb22f6dfc9b9a3265d474fd03c
shioncolor <czamy00@gmail.com>
2017-07-03 19:07:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -85,3 +85,20 @@ document.getElementById('form').select.onchange=function(){
 location.href=document.getElementById('form').select.value;
 }
 
+
+function getFileName()
+{
+return window.location.href.split('/').pop();
+}
+
+var filename=getFileName();
+var opt;
+if(filename==='other.html'){
+opt=document.querySelector('option[value="other.html"]');
+}
+else if(filename==='other2.html'){
+opt=document.querySelector('option[value="other2.html"]');
+}else{
+  opt=document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
