0c4f71da496bfdee3ce67899691abf285f82eec7
shioncolor <czamy00@gmail.com>
2017-07-03 18:14:41 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,20 +3,20 @@ var sec=time.getSeconds();
 var min=time.getMinutes();
 var hours=time.getHours();
 var days=time.getDate();
-var mont=time.getMonth();
+var month=time.getMonth();
 var year=time.getFullYear();
 return[sec,min,hours,days,month,year];
 }
 
-var now=new Data();
+var now = new Data();
 var counter=separate_time(now);
-document.getElementById('countdouwn').textCountent=
-counter[5]+'年'+
-counter[4]+'月'+
-counter[3]+'日'+
-counter[2]+'時'+
-counter[1]+'分'+
-counter[0]+'秒';
+document.getElementById('countdouwn').textContent=
+  counter[5]+'年'+
+  counter[4]+'月'+
+  counter[3]+'日'+
+  counter[2]+'時'+
+  counter[1]+'分'+
+  counter[0]+'秒';
 
 function setCookie(c_mane,value,expiredays){
 var extime=new Data().geTtime();
