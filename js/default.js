b0bec92e0999fdf8907e71e499152654a43cd316
shioncolor <czamy00@gmail.com>
2017-07-03 19:19:56 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -49,12 +49,12 @@ function getCookie(c_name){
   
   if(0<document.cookie.length){
   st=document.cookie.indexOf(c_name+"=");
-   if(st!=1){
-     st=st+c_name.length+1
+   if(st!=-1){
+     st=st+c_name.length+1;
    ed=document.cookie.indexOf(";",st);
    if(ed==1) ed=document.cookie.length;
      
-     return unescape(doument.cookie.substring(st,ed))
+     return unescape(document.cookie.substring(st,ed))
    } 
   }
   return "";
