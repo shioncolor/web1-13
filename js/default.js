04df38af5ae9e68f72cf81dfd74dec00a43b3bd2
shioncolor <czamy00@gmail.com>
2017-07-03 19:02:49 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 var separate_time=function(time){
-var sec=Math.floor((time/1000)&60);
-var min=Math.floor((time/1000/60)&60);
-var hours=Math.floor((time/1000/60/60)&24);
+var sec=Math.floor((time/1000)%60);
+var min=Math.floor((time/1000/60)%60);
+var hours=Math.floor((time/1000/60/60)%24);
 var days=Math.floor(time/1000/60/60/24);
 
 return[sec,min,hours,days];
