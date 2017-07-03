280fa375c3bb60079361ea129c2f8830fdebb280
shioncolor <czamy00@gmail.com>
2017-07-03 17:28:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -18,6 +18,37 @@ counter[2]+'時'+
 counter[1]+'分'+
 counter[0]+'秒';
 
+function setCookie(c_mane,value,expiredays){
+var extime=new Data().geTtime();
+  var cltime=new Data(extime+(60*60*24*1000*expiredays));
+  var exdata=cltime.toUTCString();
+  
+  var s="";
+  s+=c_name+"="+escape(value);
+  s+=";expires="+exdata+";";
+}else{
+s+=";";
+}
+document.cookie=s;
+}
+
+function getCookie(c_name){
+  var st="";
+  var ed="";
+  
+  if(0<doument.cookie.length){
+  st=doument.cookie.indexOf(c_name+"=");
+   if(st!=1){
+     st=st+c_name.length+1
+   ed=doument.cookie.indexOf(";",st);
+   if(ed==1) ed=doument.cookie.length;
+     
+     return unescape(doument.cookie.substring(st,ed))
+   } 
+  }
+  return "";
+}
+
 doument.getElementById('form').select.onchange=function(){
 location.href=getElementById('form').select.value;
 }
